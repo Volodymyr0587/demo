46bf631e926ce38b89b4d28373d651af2849bc3a
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-03-11 18:34:31 +0200

    Ep 15 - Make a PHP Router

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -11,10 +11,10 @@
                         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
                         <a href="/"
                             class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
-                        <a href="/about.php"
-                            class="<?= $_SERVER['REQUEST_URI'] == '/about.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
-                        <a href="/contact.php"
-                            class="<?= $_SERVER['REQUEST_URI'] == '/contact.php' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</a>
+                        <a href="/about"
+                            class="<?= $_SERVER['REQUEST_URI'] == '/about' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
+                        <a href="/contact"
+                            class="<?= $_SERVER['REQUEST_URI'] == '/contact' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</a>
 
                     </div>
                 </div>
