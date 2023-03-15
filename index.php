6ae87b62119d5efeb6c21d08235783bd8be5f125
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-03-15 22:51:28 +0200

    Ep 17 - PDO First Steps

--- index.php
+++ index.php
@@ -4,9 +4,26 @@ require 'functions.php';
 // require 'router.php';
 
 // connect to our MySQL database.
-$dsn = 'mysql:host=127.0.0.1;port=3306;dbname=demo;user=root;password=220587;charset=utf8mb4';
 
-$pdo = new PDO($dsn);
+$host = '127.0.0.1';
+$port = '3306';
+$db   = 'demo';
+$user = 'root';
+$pass = '';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+
+$opt = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+
+// $dsn = "mysql:host=$host;port=$port;dbname=$db;user=$user;charset=$charset;";
+
+$pdo = new PDO($dsn, $user, $pass, $opt);
 
 $statement = $pdo->prepare("SELECT * FROM posts");
 $statement->execute();
