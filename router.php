102a19237079b6919abe105603bc4d02a4c6a481
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-03-18 22:39:39 +0200

    Ep 22 - Render the Notes and Note Page

--- router.php
+++ router.php
@@ -4,8 +4,10 @@ $uri = parse_url($_SERVER['REQUEST_URI'])['path'];
 
 
 $routes = [
-    "/" => 'controllers/index.php',
+    '/' => 'controllers/index.php',
     '/about' => 'controllers/about.php',
+    '/notes' => 'controllers/notes.php',
+    '/note' => 'controllers/note.php',
     '/contact' => 'controllers/contact.php', 
 ];
 
--- views/partials/nav.php
+++ views/partials/nav.php
@@ -13,6 +13,8 @@
                             class="<?= urlIs('/') ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
                         <a href="/about"
                             class="<?= $_SERVER['REQUEST_URI'] == '/about' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">About</a>
+                        <a href="/notes"
+                            class="<?= $_SERVER['REQUEST_URI'] == '/notes' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Notes</a>    
                         <a href="/contact"
                             class="<?= $_SERVER['REQUEST_URI'] == '/contact' ? 'bg-gray-900 text-white' : 'text-gray-300'; ?> hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Contact</a>
 
