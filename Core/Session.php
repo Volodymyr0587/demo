25f5ddb5ddf010c90c9c8768020e4d8c766ae9dc
Volodymyr Honcharov <ladimirvm87@gmail.com>
2023-04-15 20:07:55 +0300

    Ep - 44 The PRG Pattern (and Session Flashing)

--- Core/Session.php
+++ Core/Session.php
@@ -41,6 +41,7 @@ class Session
         session_destroy();
 
         $params = session_get_cookie_params();
+
         setcookie('PHPSESSID', '', time() - 3600, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
     }
 }
